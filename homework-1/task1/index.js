735e123e35bb08e26138742d62e6d463367ed2fd
Oleksandr Pataman <oleksandr.pataman@xa.epicgames.com>
2022-12-15 18:48:58 +0100

    complete task-1

---
+++ homework-1/task1/index.js
@@ -0,0 +1,9 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+rl.on('line', (input) => {
+  console.log(input.split('').reverse().join(''));
+});
+
