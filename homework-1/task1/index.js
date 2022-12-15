50751ee256749aba4effd5bed2a415532f9a966f
Oleksandr Pataman <oleksandr.pataman@xa.epicgames.com>
2022-12-15 19:56:23 +0100

    complete task-3

--- homework-1/task1/index.js
+++ homework-1/task1/index.js
@@ -1,9 +1,5 @@
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
-
-const rl = readline.createInterface({ input, output });
-
-rl.on('line', (input) => {
-  console.log(input.split('').reverse().join(''));
+require("@babel/register")({
+  presets: ["@babel/preset-env"]
 });
 
+module.exports = require('./script.js')
--- homework-1/task2/index.js
+++ homework-1/task2/index.js
@@ -1,19 +1,5 @@
-const csv = require("csvtojson");
-const fs = require("fs");
-const { pipeline } = require("stream");
+require("@babel/register")({
+  presets: ["@babel/preset-env"]
+});
 
-const csvFilePath = "./task2/csv/nodejs-hw1-ex1.csv";
-const txtFilePath = "./task2/txt/nodejs-hw1-ex1.txt";
-
-pipeline(
-  fs.createReadStream(csvFilePath),
-  csv(),
-  fs.createWriteStream(txtFilePath),
-  (error) => {
-    if (error) {
-      console.error("Something went wrong:\n", error);
-    } else {
-      console.log("File processed successfully");
-    }
-  }
-);
+module.exports = require('./script.js')
