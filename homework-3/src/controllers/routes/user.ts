f914a1a03e109e8c8f41e2b3bd7ee21098cc117d
Oleksandr Pataman <oleksandr.pataman@xa.epicgames.com>
2023-01-16 10:42:27 +0100

    group routes by logic modules

--- homework-3/src/controllers/routes/user.ts
+++ homework-3/src/controllers/routes/user.ts
@@ -10,11 +10,19 @@ import { validators } from "../middlewares/validators";
 import UserService from "../../services/user";
 
 export default (app: Router) => {
-  app.get("/", async (req, res) => {
-    return res.send(await UserService.getAllUsers());
-  });
+  app
+    .route("/")
+    .get(async (req, res) => {
+      return res.send(await UserService.getAllUsers());
+    })
+    .post(validators.addNewUser, async (req, res) => {
+      const result = await UserService.addUser(req.body);
+      return result
+        ? res.send(result)
+        : res.status(HTTP_CODE_BAD_REQUEST).send(MESSAGE_SOMETHING_WRONG);
+    });
 
-  app.get("/search", async (req, res) => {
+  app.route("/search").get(async (req, res) => {
     const { loginSubstring = "", limit = AUTO_SUGGEST_USERS_DEFAULT_LIMIT } =
       req.query;
 
@@ -26,39 +34,32 @@ export default (app: Router) => {
     );
   });
 
-  app.get("/:userId", async (req, res) => {
-    const result = await UserService.getUser(req.params.userId);
-    return result
-      ? res.send(result)
-      : res.status(HTTP_CODE_NOT_FOUND).send(MESSAGE_NOT_FOUND);
-  });
-
-  app.post("/", validators.addNewUser, async (req, res) => {
-    const result = await UserService.addUser(req.body);
-    return result
-      ? res.send(result)
-      : res.status(HTTP_CODE_BAD_REQUEST).send(MESSAGE_SOMETHING_WRONG);
-  });
-
-  app.put("/:userId", validators.updateExistingUser, async (req, res) => {
-    const {
-      params: { userId },
-      body,
-    } = req;
-    const [, result] = await UserService.updateUser(userId, body);
-    return result
-      ? res.send(result)
-      : res.status(HTTP_CODE_BAD_REQUEST).send(MESSAGE_SOMETHING_WRONG);
-  });
-
-  app.delete("/:userId", async (req, res) => {
-    const {
-      params: { userId },
-      body,
-    } = req;
-    const [, result] = await UserService.deleteUser(userId, body);
-    return result
-      ? res.send(result)
-      : res.status(HTTP_CODE_BAD_REQUEST).send(MESSAGE_SOMETHING_WRONG);
-  });
+  app
+    .route("/:userId")
+    .get(async (req, res) => {
+      const result = await UserService.getUser(req.params.userId);
+      return result
+        ? res.send(result)
+        : res.status(HTTP_CODE_NOT_FOUND).send(MESSAGE_NOT_FOUND);
+    })
+    .put(validators.updateExistingUser, async (req, res) => {
+      const {
+        params: { userId },
+        body,
+      } = req;
+      const [, result] = await UserService.updateUser(userId, body);
+      return result
+        ? res.send(result)
+        : res.status(HTTP_CODE_BAD_REQUEST).send(MESSAGE_SOMETHING_WRONG);
+    })
+    .delete(async (req, res) => {
+      const {
+        params: { userId },
+        body,
+      } = req;
+      const [, result] = await UserService.deleteUser(userId, body);
+      return result
+        ? res.send(result)
+        : res.status(HTTP_CODE_BAD_REQUEST).send(MESSAGE_SOMETHING_WRONG);
+    });
 };
